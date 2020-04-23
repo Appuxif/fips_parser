be08e41fd7a911fe729d8e4806aa2b237810001e
Appuxif <smokost@gmail.com>
2020-04-23 13:45:16 +0700

    Init

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
-from config import SECRET_KEY, USE_SQLITE3, DBUSERNAME, DBPASS, DBNAME, DBHOST
+from config import USE_SQLITE3, DBUSERNAME, DBPASS, DBNAME, DBHOST
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
