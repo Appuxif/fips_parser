93f94eba25ede445189a9adcc9826d43e5a13f8f
Appuxif <smokost@gmail.com>
2020-04-23 17:45:32 +0700

    Init

--- webapp/webapp/wsgi.py
+++ webapp/webapp/wsgi.py
@@ -10,10 +10,11 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 import sys
-from django.core.wsgi import get_wsgi_application
-
 sys.path.append('/var/www/fips_parser')
 sys.path.append('/var/www/fips_parser/venv/lib/python3.7/site-packages')
+
+from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webapp.settings')
 
 application = get_wsgi_application()
