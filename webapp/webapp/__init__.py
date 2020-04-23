5e49bfe423794feee9ddaf5ba533f86cd7f80311
Appuxif <smokost@gmail.com>
2020-04-23 14:00:33 +0700

    Init

--- webapp/webapp/__init__.py
+++ webapp/webapp/__init__.py
@@ -1,4 +1,4 @@
-from config import USE_SQLITE3
-if not USE_SQLITE3:
-    import pymysql
-    pymysql.install_as_MySQLdb()
+# from config import USE_SQLITE3
+# if not USE_SQLITE3:
+#     import pymysql
+#     pymysql.install_as_MySQLdb()
