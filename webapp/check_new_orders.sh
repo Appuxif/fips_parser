65fdb9a7d5066d40d3382ef7f441ebe4b36dcd60
Appuxif <smokost@gmail.com>
2020-05-01 20:25:10 +0700

    parser processor added

--- webapp/check_new_orders.sh
+++ webapp/check_new_orders.sh
@@ -1,1 +1,2 @@
+cd /var/www/fips_parser/webapp
 sudo ../venv/bin/python -c "from orders_parser import *; p = OrdersParser(ORDERS_URL, 'orders'); p.check_new_documents()"
--- webapp/check_new_registers.sh
+++ webapp/check_new_registers.sh
@@ -1,1 +1,2 @@
+cd /var/www/fips_parser/webapp
 sudo ../venv/bin/python -c "from registers_parser import *; p = RegistersParser(REGISTERS_URL, 'registers'); p.check_new_documents()"
