016c3704f82f208e6fe563561f6b400705c4f61c
Appuxif <smokost@gmail.com>
2020-05-31 15:45:03 +0700

    fixes

--- webapp/interface/templatetags/last_documents.py
+++ webapp/interface/templatetags/last_documents.py
@@ -10,9 +10,11 @@ class LastDocumentsNode(template.Node):
     def __init__(self):
         self.orders_count = OrdersDocument.objects.count()
         self.registers_count = RegistersDocument.objects.count()
-        self.orders = OrdersDocument.objects.filter(document_parsed=True).order_by('-date_parsed')
+        # self.orders = OrdersDocument.objects.filter(document_parsed=True).order_by('-date_parsed')
+        self.orders = OrdersDocument.objects.filter(document_parsed=True)
         self.orders_parsed = self.orders.count()
-        self.registers = RegistersDocument.objects.filter(document_parsed=True).order_by('-date_parsed')
+        # self.registers = RegistersDocument.objects.filter(document_parsed=True).order_by('-date_parsed')
+        self.registers = RegistersDocument.objects.filter(document_parsed=True)
         self.registers_parsed = self.registers.count()
 
     def __repr__(self):
@@ -22,8 +24,8 @@ class LastDocumentsNode(template.Node):
         # last_documents_list = list(self.orders[:10]) + list(self.registers[:10])
         # last_documents_list = zip(self.orders[:5], self.registers[:5])
         # context['last_documents_list'] = sorted(last_documents_list, key=lambda x: x.id)
-        orders_count = self.orders.count()
-        registers_count = self.registers.count()
+        # orders_count = self.orders.count()
+        # registers_count = self.registers.count()
         # context['last_orders'] = self.orders[:5]
         # context['last_registers'] = self.registers[:5]
         context['orders_percent'] = str(self.orders_parsed) + ' из ' + str(self.orders_count)
